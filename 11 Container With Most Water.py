4a6cf1827a7587a52eb69416931963a90462e3e1
rndmjhn <johnmelliott95@gmail.com>
2024-05-05 22:57:37 -0500

    daily update 2

--- 11 Container With Most Water.py
+++ 11 Container With Most Water.py
@@ -1,34 +1,24 @@
 heights = [1,8,6,2,5,4,8,3,7]
 output = 49
 
-
-
 class Solution:
     def maxArea(self, height: list[int]) -> int:
-        currentmax = [0, 0, 0]
+        maxarea = 0
         start = 0
-        end = len(height) -1
-        flipper = False
+        end = len(height) - 1
+
         while start < end:
-            print(currentmax)
-            if (((end + 1) - (start + 1)) * min(height[start], height[end])) > currentmax[2]:
-                currentmax[0], currentmax[1], currentmax[2] = start, end, ((end + 1) - (start + 1)) * min(height[start], height[end])
-                if flipper:
-                    start += 1
-                    flipper = False
-                else:
-                    end -= 1
-                    flipper = True
-                continue
+            area = min(height[start], height[end]) * (end - start)
+            maxarea = max(maxarea, area)
+
+            if height[start] < height[end]:
+                start += 1
             else:
-                if flipper:
-                    start += 1
-                    flipper = False
-                else:
-                    end -= 1
-                    flipper = True
-                continue
-            print(currentmax)
+                end -= 1
+
+        return maxarea
+
+
 
 
 test = Solution()
