4d1890e588327a9eaded1bff5287c0644249102b
John Elliott <johnmelliott95@gmail.com>
2024-06-05 18:06:31 -0500

    Update - Finished 104

--- 104 Maximum Depth of Binary Tree.py
+++ 104 Maximum Depth of Binary Tree.py
@@ -1,12 +1,40 @@
+import json
 
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+    def __repr__(self):
+        # Shrink the long json string by removing unnecessary lines
+        t = '\n'.join([l for l in self.json_repr.split('\n')
+            if '[' not in l and ']' not in l])
+        return t.replace(',','')
+
+    @property
+    def json_repr(self):
+        if self.val is None:
+            return 'None'
+        # Recursively construct a json-compliant representation
+        if self.left is None:
+            text = f"[{self.val}]"
+        else:
+            text = f"[{self.val}, [{self.left.json_repr}, {self.right.json_repr}]]"
+        return json.dumps(json.loads(text), indent=1)
+
+    def from_list(l):
+            nodes = [TreeNode(v) for v in l]
+            kids = nodes[::-1]
+            root = kids.pop()
+            for node in nodes:
+                if node:
+                    if kids:
+                        node.left = kids.pop()
+                    if kids:
+                        node.right = kids.pop()
+            return root
 
 
 root1 = [3,9,20,None,None,15,7]
@@ -18,11 +46,10 @@ class Solution:
            return 0
         left_depth = self.maxDepth(root.left)
         right_depth = self.maxDepth(root.right)
-
         return max(left_depth,right_depth) + 1
                        
     
 
 test = Solution()
-print(test.maxDepth(root1))
-print(test.maxDepth(root2))
+print(test.maxDepth(TreeNode.from_list(root1)))
+print(test.maxDepth(TreeNode.from_list(root2)))
